8b2c4480112ab3d4e1726fbc8eeea9ed24bf25dd
S1lentSt0rm <lutoshkinstep@gmail.com>
2023-07-05 19:51:39 +0700

    scene scheme based on mvc
    - Added hierarchy of base elements to the game scene

---
+++ Assets/Application.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Application : MonoBehaviour
+{
+    public Model model;
+    public View view;
+    public Controller controller;
+}
